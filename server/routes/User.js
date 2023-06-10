d5f7f5e4f651f358d39116e398f6d2afd9a64104
simranjit singh <simranjits008@gmail.com>
2023-06-10 13:04:57 +0530

    package update

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,20 +1,20 @@
 // Import the required modules
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Import the required controllers and middleware functions
 const {
   login,
   signup,
-  sendotp,
+  sendOTP,
   changePassword,
-} = require("../controllers/Auth")
+} = require("../controllers/Auth");
 const {
   resetPasswordToken,
   resetPassword,
-} = require("../controllers/ResetPassword")
+} = require("../controllers/ResetPassword");
 
-const { auth } = require("../middlewares/auth")
+const { auth } = require("../middlewares/auth");
 
 // Routes for Login, Signup, and Authentication
 
@@ -23,26 +23,26 @@ const { auth } = require("../middlewares/auth")
 // ********************************************************************************************************
 
 // Route for user login
-router.post("/login", login)
+router.post("/login", login);
 
 // Route for user signup
-router.post("/signup", signup)
+router.post("/signup", signup);
 
 // Route for sending OTP to the user's email
-router.post("/sendotp", sendotp)
+router.post("/sendotp", sendotp);
 
 // Route for Changing the password
-router.post("/changepassword", auth, changePassword)
+router.post("/changepassword", auth, changePassword);
 
 // ********************************************************************************************************
 //                                      Reset Password
 // ********************************************************************************************************
 
 // Route for generating a reset password token
-router.post("/reset-password-token", resetPasswordToken)
+router.post("/reset-password-token", resetPasswordToken);
 
 // Route for resetting user's password after verification
-router.post("/reset-password", resetPassword)
+router.post("/reset-password", resetPassword);
 
 // Export the router for use in the main application
-module.exports = router
+module.exports = router;
--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,14 +1,14 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
-exports.uploadImageToCloudinary = async (file, folder, height, quality) {
-    const option = {folder};
-    if(height) {
-        option.height = height;
-    }
-    if(quality) {
-        option.quality = quality;
-    }
-    option.resource_type = "auto";
+exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
+  const option = { folder };
+  if (height) {
+    option.height = height;
+  }
+  if (quality) {
+    option.quality = quality;
+  }
+  option.resource_type = "auto";
 
-    return await cloudinary.uploader.upload(file.tempFilePath, option);
-}
+  return await cloudinary.uploader.upload(file.tempFilePath, option);
+};
