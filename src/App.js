17d83b2d80e2fdf7f1b3963d2cdda48575d405d9
Firdosh01 <firdosh7392@gmail.com>
2023-06-22 03:53:03 -0500

    update about page route

--- src/App.js
+++ src/App.js
@@ -63,14 +63,7 @@ function App() {
           }
         />
 
-        <Route
-          path="about"
-          element={
-            <OpenRoute>
-              <About />
-            </OpenRoute>
-          }
-        />
+        <Route path="about" element={ <About /> } />
 
         <Route path='/contact' element={<Contact />} />
 
