c651b0b21f9aa0aa2c52742384fbfef6021a280c
Firdosh01 <firdosh7392@gmail.com>
2023-06-24 21:19:02 -0500

    update button component

--- src/Components/common/iconBtn.js
+++ src/Components/common/iconBtn.js
@@ -13,11 +13,14 @@ const IconBtn = ({
     <button 
     disabled={disabled}
     onClick={onclick}
+    className={`flex items-center ${
+        outline ? "border border-yellow-50 bg-transparent" : "bg-yellow-50"
+      } cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 ${customClasses}`}
     type={type}>
         {
             children ? (
                 <>
-                    <span>
+                    <span className={`${outline && "text-yellow-50"}`}>
                         {text}
                     </span>
                     {children}
