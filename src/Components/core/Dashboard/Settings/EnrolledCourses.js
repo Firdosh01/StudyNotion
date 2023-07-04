37aaae257f9cf0f056ec55a8a0295375139d5d45
Firdosh01 <firdosh7392@gmail.com>
2023-07-03 21:53:26 -0500

    EnrolledCourses implemented

--- src/Components/core/Dashboard/Settings/EnrolledCourses.js
+++ src/Components/core/Dashboard/Settings/EnrolledCourses.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { getUserEnrolledCourses } from '../../../../services/operations/profileAPI';
+import ProgressBar from '@ramonak/react-progress-bar';
 
 function EnrolledCourses() {
 
@@ -24,7 +25,52 @@ function EnrolledCourses() {
   return (
     <div className='text-white'>
         <div>Enrolled Courses</div>
-      
+      {
+        !enrolledCourses ?
+        (
+            <div>
+                Loading...
+            </div>
+        ) :
+        !enrolledCourses.length ? 
+        (
+            <p>You have not enrolled in any course yet</p>
+        ) :
+        (
+            <div>
+                <div>
+                    <p>Course Name</p>
+                    <p>Durations</p>
+                    <p>Progress</p>
+                </div>
+                {
+                    enrolledCourses.map((course,index) => (
+                        <div>
+                            <div>
+                                <img src={course.thumbnail} alt="" />
+                                <div>
+                                    <p>{course.courseName}</p>
+                                    <p>{course.courseDescription}</p>
+                                </div>
+                            </div>
+                            <div>
+                                {course?.totalDuration}
+                            </div>
+                            
+                            <div>
+                                <p>Progress: {course.progressPercentage || 0}% </p>
+                                <ProgressBar
+                                        completed={course.progressPercentage || 0}
+                                        height='8px'
+                                        isLabelVisible={false}
+                                        />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        )
+      }
     </div>
   )
 }
