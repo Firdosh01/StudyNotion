f1e17ab6913e956d390b25fdd0549f02ba4b4e9f
Md Firdosh Ansari <115987354+Firdosh01@users.noreply.github.com>
2023-08-05 11:37:07 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://cute-panda-a06061.netlify.app",
 		credentials:true,
 	})
 )
